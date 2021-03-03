a4c960c7cd406fb0fd1103aac86d5365c30286d3
John Keiser <jkeiser@lynxtool.com>
2021-03-02 22:15:54 -0800

    Pass "capacity" straight to iterate, support std::string

--- tests/ondemand/ondemand_parse_api_tests.cpp
+++ tests/ondemand/ondemand_parse_api_tests.cpp
@@ -18,35 +18,97 @@ namespace parse_api_tests {
     return true;
   }
 
+  bool parser_iterate_padded() {
+    TEST_START();
+    ondemand::parser parser;
+    const char json_str[] = "12\0                              "; // 32 padding
+    static_assert(sizeof(json_str) > sizeof(const char*));
+
+    {
+      auto doc = parser.iterate(json_str, strlen(json_str), sizeof(json_str));
+      ASSERT_SUCCESS( doc.get_double() );
+    }
+
+    {
+      const uint8_t* json = reinterpret_cast<const uint8_t*>(json_str);
+      auto doc = parser.iterate(json, strlen(json_str), sizeof(json_str));
+      ASSERT_SUCCESS( doc.get_double() );
+    }
+
+    {
+      std::string_view json(json_str);
+      auto doc = parser.iterate(json, sizeof(json_str));
+      ASSERT_SUCCESS( doc.get_double() );
+    }
+
+    {
+      std::string json = "12";
+      json.reserve(json.length() + SIMDJSON_PADDING);
+      auto doc = parser.iterate(json);
+      ASSERT_SUCCESS( doc.get_double() );
+    }
+
+    TEST_SUCCEED();
+  }
+
   bool parser_iterate_padded_string_view() {
     TEST_START();
     ondemand::parser parser;
+    const char json_str[] = "12\0                                ";
+    static_assert(sizeof(json_str) > sizeof(const char*));
 
-    padded_string_view json("12                                ", 2);
-    auto doc = parser.iterate(json);
-    ASSERT_SUCCESS( doc.get_double() );
+    {
+      padded_string_view json(json_str, sizeof(json_str));
+      auto doc = parser.iterate(json);
+      ASSERT_SUCCESS( doc.get_double() );
+    }
 
-    json = padded_string_view(std::string_view("12                                ", 2));
-    doc = parser.iterate(json);
-    ASSERT_SUCCESS( doc.get_double() );
+    {
+      padded_string_view json(std::string_view(json_str), sizeof(json_str));
+      auto doc = parser.iterate(json);
+      ASSERT_SUCCESS( doc.get_double() );
+    }
 
-    doc = parser.iterate(padded_string_view("12                                ", 2));
-    ASSERT_SUCCESS( doc.get_double() );
-    return true;
+    {
+      auto doc = parser.iterate(padded_string_view(json_str, strlen(json_str), sizeof(json_str)));
+      ASSERT_SUCCESS( doc.get_double() );
+    }
+
+    {
+      std::string json = "12";
+      json.reserve(json.length() + SIMDJSON_PADDING);
+      auto doc = parser.iterate(padded_string_view(json));
+      ASSERT_SUCCESS( doc.get_double() );
+    }
+
+    TEST_SUCCEED();
   }
 
-  bool parser_iterate_promise_padded() {
+  bool parser_iterate_insufficient_padding() {
     TEST_START();
     ondemand::parser parser;
+    constexpr char json_str[] = "12\0                             "; // 31 padding
+    static_assert(sizeof(json_str) > sizeof(const char*));
 
-    auto doc = parser.iterate(promise_padded("12                                ", 2));
-    ASSERT_SUCCESS( doc.get_double() );
+    {
+      ASSERT_ERROR( parser.iterate(json_str, strlen(json_str), sizeof(json_str)), INSUFFICIENT_PADDING );
+      ASSERT_ERROR( parser.iterate(json_str, strlen(json_str), strlen(json_str)), INSUFFICIENT_PADDING );
+    }
 
-    std::string_view json("12                                ", 2);
-    doc = parser.iterate(promise_padded(json));
-    ASSERT_SUCCESS( doc.get_double() );
+    {
+      std::string_view json(json_str);
+      ASSERT_ERROR( parser.iterate(json, sizeof(json_str)), INSUFFICIENT_PADDING );
+      ASSERT_ERROR( parser.iterate(json, strlen(json_str)), INSUFFICIENT_PADDING );
+    }
 
-    return true;
+    {
+      std::string json = "12";
+      ASSERT_ERROR( parser.iterate(json), INSUFFICIENT_PADDING );
+      // It's actually kind of hard to allocate "just enough" capacity, since the string tends
+      // to grow more than you tell it to.
+    }
+
+    TEST_SUCCEED();
   }
 
 #if SIMDJSON_EXCEPTIONS
@@ -55,14 +117,15 @@ namespace parse_api_tests {
     ondemand::parser parser;
     auto doc = parser.iterate(BASIC_JSON);
     simdjson_unused ondemand::array array = doc;
-    return true;
+    TEST_SUCCEED();
   }
 #endif // SIMDJSON_EXCEPTIONS
 
   bool run() {
     return parser_iterate() &&
+           parser_iterate_padded() &&
            parser_iterate_padded_string_view() &&
-           parser_iterate_promise_padded() &&
+           parser_iterate_insufficient_padding() &&
 #if SIMDJSON_EXCEPTIONS
            parser_iterate_exception() &&
 #endif // SIMDJSON_EXCEPTIONS
